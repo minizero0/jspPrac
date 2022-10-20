d3164de0afc44f9f325382425308d19f4483cbc8
yeongmin <dudals1069@gmail.com>
2022-10-20 15:21:51 +0900

    jdbc_test

--- day1020_a/src/main/java/com/sist/exam01/DriverTest.java
+++ day1020_a/src/main/java/com/sist/exam01/DriverTest.java
@@ -1,7 +1,26 @@
 package com.sist.exam01;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.sist.db.ConnectionProvider;
+
 public class DriverTest {
 	public static void main(String[] args) {
-		
+		Connection conn;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			String url = "jdbc:oracle:thin:@localhost:1521:XE";
+			String usr = "c##madang";
+			String pwd = "madang";
+			
+			conn = DriverManager.getConnection(url,usr,pwd);
+			System.out.println("success");
+		}catch (SQLException e) {
+			System.out.println("SQL예외발생:"+e.getMessage());
+		}catch (Exception e) {
+			System.out.println("예외발생:"+e.getMessage());
+		}
 	}
 }
