94296531ad38f53a126bdb65f5bd5e4475e80645
yeongmin <dudals1069@gmail.com>
2022-10-24 12:56:58 +0900

    BoardTest

---
+++ boardTest/src/main/java/com/sist/dao/BoardDAO.java
@@ -0,0 +1,108 @@
+package com.sist.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import com.sist.vo.BoardVO;
+
+public class BoardDAO {
+	
+	public int insertBoard(BoardVO bv) {
+		int re = 0;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			String sql = "insert into board(no,writer,pwd,title,content) values(?,?,?,?,?)";
+			Context context = new InitialContext();
+			DataSource ds = (DataSource)context.lookup("java:/comp/env/mydb");
+			conn = ds.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, bv.getNo());
+			pstmt.setString(2, bv.getWriter());
+			pstmt.setString(3, bv.getPwd());
+			pstmt.setString(4, bv.getTitle());
+			pstmt.setString(5, bv.getContent());
+			re = pstmt.executeUpdate();
+			
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		}finally {
+			if(conn!=null){try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}}
+			if(pstmt!=null){try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}}
+		}
+		
+		return re;
+	}
+	
+	public ArrayList<BoardVO> listBoard(){
+		ArrayList<BoardVO> list = new ArrayList<>();
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+			String sql = "select * from board";
+			Context context = new InitialContext();
+			DataSource ds = (DataSource)context.lookup("java:/comp/env/mydb");
+			conn = ds.getConnection();
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql);
+			while(rs.next()) {
+				BoardVO bv = new BoardVO();
+				bv.setNo(rs.getInt("no"));
+				bv.setWriter(rs.getString("writer"));
+				bv.setPwd(rs.getString("pwd"));
+				bv.setTitle(rs.getString("title"));
+				bv.setContent(rs.getString("content"));
+				bv.setRegdate(rs.getDate("regdate"));
+				bv.setHit(rs.getInt("hit"));
+				list.add(bv);
+			}
+			
+		}catch (Exception e) {
+			// TODO: handle exception
+		}finally {
+			if(rs!=null) {try {
+				rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}}
+			if(stmt!=null) {try {
+				stmt.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}}
+			if(conn!=null) {try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}}
+			
+		}
+		
+		
+		return list;
+	}
+
+}
---
+++ boardTest/src/main/java/com/sist/vo/BoardVO.java
@@ -0,0 +1,16 @@
+package com.sist.vo;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class BoardVO {
+	private int no;
+	private String Writer;
+	private String pwd;
+	private String title;
+	private String content;
+	private Date regdate;
+	private int hit;
+}
