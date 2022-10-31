2a080cc8d3d18c852df30ca469f4e790c2e9faf7
yeongmin <dudals1069@gmail.com>
2022-10-31 11:39:59 +0900

    mvcTest

--- 1031_mvcTest/src/main/java/com/sist/action/ListBoardAction.java
+++ 1031_mvcTest/src/main/java/com/sist/action/ListBoardAction.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.sist.dao.BoardDAO;
 import com.sist.vo.BoardVO;
@@ -23,10 +24,23 @@ public class ListBoardAction implements SistAction{
 		if(request.getParameter("pageNUM") != null) {
 			pageNUM = Integer.parseInt(request.getParameter("pageNUM"));	
 		}
+		HttpSession session = request.getSession();
+		if(session.getAttribute("keyword")!=null){
+			searchColumn = (String)session.getAttribute("searchColumn");
+			keyword = (String)session.getAttribute("keyword");
+		}
 		
+		if(request.getParameter("keyword")!=null){
+			searchColumn = request.getParameter("searchColumn");
+			keyword = request.getParameter("keyword");
+		}
 		ArrayList<BoardVO> list = dao.findAll(pageNUM, searchColumn, keyword);
 		request.setAttribute("list", list);
 		request.setAttribute("totalPage", dao.totalPage);
+		
+		session.setAttribute("searchColumn", searchColumn);
+		session.setAttribute("keyword", keyword);
+		
 		
 		
 		return "listBoard.jsp";
--- 1031_mvcTest/src/main/webapp/insertBoard.jsp
+++ 1031_mvcTest/src/main/webapp/insertBoard.jsp
@@ -8,5 +8,16 @@
 </head>
 <body>
 	<h2>게시물 등록</h2>
+	<hr>
+	<form action="insertBoard.do" method="post" enctype="multipart/form-data">
+		작성자 : <input type = "text" name = "writer" required="required"><br> 
+		글암호 : <input type = "password" name = "pwd" required="required"><br>
+		글제목 : <input type = "text" name = "title" required="required"><br>
+		글내용 : 
+		<textarea rows="10" cols="60" name = "content" required="required"></textarea><br>
+		첨부파일 : <input type = "file" name = "uploadFile"><br>
+		<input type = "submit" value = "등록">
+		<input type = "reset" value = "취소">
+	</form>
 </body>
 </html>
