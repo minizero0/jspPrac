06908bb680f00ae406e47a3ac6d694c13165b951
yeongmin <dudals1069@gmail.com>
2022-11-04 11:18:51 +0900

    Ajax(url,data,success)

---
+++ day1104/src/main/java/com/sist/dao/BookDAO.java
@@ -0,0 +1,241 @@
+package com.sist.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.spi.DirStateFactory.Result;
+import javax.sql.DataSource;
+
+import com.sist.vo.BookVO;
+
+public class BookDAO {
+	private static BookDAO dao;
+	
+	public static BookDAO getInstance() {
+		if(dao == null) 
+			dao = new BookDAO();
+		return dao;
+	}
+	
+	
+	private BookDAO() {
+		
+	}
+	
+	public int maxBookid() {
+		int bookid = 0;
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		String sql = "select nvl(max(bookid),0) + 1 from book";
+		try {
+			Context context = new InitialContext();
+			DataSource ds = (DataSource)context.lookup("java:/comp/env/mydb");
+			conn = ds.getConnection();
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql);
+			while(rs.next()) {
+				bookid = rs.getInt(1);
+			}
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		}finally {
+			if(rs!=null) {try {
+				rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}}
+			if(stmt!=null) {try {
+				stmt.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}}
+			if(conn!=null) {try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}}
+		}
+		
+		return bookid;
+	}
+	
+	public int insertBook(BookVO bv) {
+		
+		int re = 0;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = "insert into book values(?,?,?,?)";
+		try {
+			Context context = new InitialContext();
+			DataSource ds = (DataSource)context.lookup("java:/comp/env/mydb");
+			conn = ds.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, bv.getBookid());
+			pstmt.setString(2, bv.getBookname());
+			pstmt.setString(3, bv.getPublisher());
+			pstmt.setInt(4, bv.getPrice());
+			re = pstmt.executeUpdate(); 
+			
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		}finally {
+			if(conn!=null) {try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}}
+			if(pstmt!=null) {try {
+				pstmt.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}}
+		}
+		
+		
+		return re;
+	}
+	
+	
+	public int updateBook(BookVO bv) {
+		int re = 0;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = "update book set bookname = ?, publisher = ?, price = ? where bookid = ?";
+		try {
+			Context context = new InitialContext();
+			DataSource ds = (DataSource)context.lookup("java:/comp/env/mydb");
+			conn = ds.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, bv.getBookname());
+			pstmt.setString(2, bv.getPublisher());
+			pstmt.setInt(3, bv.getPrice());
+			pstmt.setInt(4, bv.getBookid());
+			re = pstmt.executeUpdate(); 
+			
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		}finally {
+			if(conn!=null) {try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}}
+			if(pstmt!=null) {try {
+				pstmt.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}}
+		}
+		
+		
+		return re;
+	}
+	
+	public BookVO findByBookid(int bookid) {
+		BookVO bv = null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "select * from book where bookid = ?";
+		try {
+			Context context = new InitialContext();
+			DataSource ds = (DataSource)context.lookup("java:/comp/env/mydb");
+			conn = ds.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, bookid);
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				bv = new BookVO();
+				bv.setBookid(rs.getInt("bookid"));
+				bv.setBookname(rs.getString("bookname"));
+				bv.setPublisher(rs.getString("publisher"));
+				bv.setPrice(rs.getInt("price"));
+			}
+			
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		}finally {
+			if(conn!=null) {try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}}
+			if(pstmt!=null) {try {
+				pstmt.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}}
+			if(rs!=null) {try {
+				rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}}
+		}
+		
+		return bv;
+	}
+	
+	
+	public ArrayList<BookVO> findAll(){
+		ArrayList<BookVO> list = new ArrayList<>();
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		String sql = "select * from book";
+		try {
+			Context context = new InitialContext();
+			DataSource ds = (DataSource)context.lookup("java:/comp/env/mydb");
+			conn = ds.getConnection();
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql);
+			while(rs.next()) {
+				BookVO bv = new BookVO();
+				bv.setBookid(rs.getInt("bookid"));
+				bv.setBookname(rs.getString("bookname"));
+				bv.setPublisher(rs.getString("publisher"));
+				bv.setPrice(rs.getInt("price"));
+				list.add(bv);
+			}
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		}finally {
+			if(conn!=null) {try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}}
+			if(stmt!=null) {try {
+				stmt.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}}
+			if(rs!=null) {try {
+				rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}}
+		}
+		return list;
+	}
+	
+}
---
+++ day1104/src/main/java/com/sist/servlet/ListDept.java
@@ -0,0 +1,54 @@
+package com.sist.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.sist.dao.DeptDAO;
+import com.sist.vo.DeptVO;
+
+/**
+ * Servlet implementation class ListDept
+ */
+@WebServlet("/dept/List")
+public class ListDept extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ListDept() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json;charset=utf-8");
+		DeptDAO dao = DeptDAO.getInstance();
+		ArrayList<DeptVO> list = dao.findAll();
+		Gson gson = new Gson();
+		String str = gson.toJson(list);
+		PrintWriter out = response.getWriter();
+		out.print(str);
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
