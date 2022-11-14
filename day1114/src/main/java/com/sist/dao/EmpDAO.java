cebfe1c9f275815cab18bd0fcbaf38e2c219eb70
yeongmin <dudals1069@gmail.com>
2022-11-14 12:10:51 +0900

    jqGrid

---
+++ day1114/src/main/java/com/sist/dao/EmpDAO.java
@@ -0,0 +1,124 @@
+package com.sist.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import com.sist.vo.DeptVO;
+import com.sist.vo.EmpVO;
+
+public class EmpDAO {
+	private static EmpDAO dao;
+	
+	public static EmpDAO getInstance() {
+		if (dao == null)
+			dao = new EmpDAO();
+		return dao;
+	}
+	
+	private EmpDAO() {
+		
+	}
+	//사원번호로 사원찾기
+	public EmpVO findByEno(int eno) {
+		EmpVO ev = null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "select * from emp where eno = ?";
+		try {
+			Context context = new InitialContext();
+			DataSource ds = (DataSource)context.lookup("java:/comp/env/mydb");
+			conn = ds.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, eno);
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				ev = new EmpVO();
+				ev.setEno(rs.getInt("eno"));
+				ev.setEname(rs.getString("ename"));
+				ev.setDno(rs.getInt("dno"));
+				ev.setSalary(rs.getInt("salary"));
+				ev.setPhone(rs.getString("phone"));
+				ev.setEmail(rs.getString("email"));
+			}
+			
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		}finally {
+			if(conn!=null) {try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}}
+			if(pstmt!=null) {try {
+				pstmt.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}}
+			if(rs!=null) {try {
+				rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}}
+		}
+		
+		return ev;
+	}
+	
+	public ArrayList<EmpVO> findAll(){
+		ArrayList<EmpVO> list = new ArrayList<>();
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		String sql = "select * from emp";
+		try {
+			Context context = new InitialContext();
+			DataSource ds = (DataSource)context.lookup("java:/comp/env/mydb");
+			conn = ds.getConnection();
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql);
+			while(rs.next()) {
+				EmpVO ev = new EmpVO();
+				ev.setEno(rs.getInt("eno"));
+				ev.setEname(rs.getString("ename"));
+				ev.setDno(rs.getInt("dno"));
+				ev.setSalary(rs.getInt("salary"));
+				list.add(ev);
+			}
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		}finally {
+			if(conn!=null) {try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}}
+			if(stmt!=null) {try {
+				stmt.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}}
+			if(rs!=null) {try {
+				rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}}
+		}
+		return list;
+	}
+
+}
---
+++ day1114/src/main/java/com/sist/servlet/GetBooks.java
@@ -0,0 +1,52 @@
+package com.sist.servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sist.dao.BookDAO;
+import com.sist.vo.BookVO;
+
+/**
+ * Servlet implementation class GetBooks
+ */
+@WebServlet("/GetBooks")
+public class GetBooks extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GetBooks() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		BookDAO dao = BookDAO.getInstance();
+		ArrayList<BookVO> list = dao.findAll();
+		String str = "<rows>";
+		str += "<page>1</page>";
+		str += "<total>1</total>";
+		str += "<records>10</records>";
+		str += "</rows>";
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
